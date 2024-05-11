6577b1b22614507ac4e9594d7638f45ebbc8f757
Leo Wang <ab0988956087@gmail.com>
2024-05-11 17:39:11 +0800

    configure noice for firenvim and macro message

--- init.lua
+++ init.lua
@@ -31,16 +31,21 @@ require('lazy').setup({
   {
     "folke/noice.nvim",
     event = "VeryLazy",
+    cond = function()
+      return not vim.g.started_by_firenvim
+    end,
     opts = {
       -- add any options here
+      routes = {
+        {
+          view = "notify",
+          filter = { event = "msg_showmode" },
+        },
+      },
     },
     dependencies = {
       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
       "MunifTanjim/nui.nvim",
-      -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
-      "rcarriga/nvim-notify",
     }
   },
   "f-person/git-blame.nvim",
