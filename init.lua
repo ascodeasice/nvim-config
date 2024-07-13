b847c3e1e7f3ae20d0166926a224cd9e326fb743
Leo Wang <ab0988956087@gmail.com>
2024-07-13 23:54:14 +0800

    set up colorizer

--- init.lua
+++ init.lua
@@ -27,6 +27,12 @@ vim.opt.rtp:prepend(lazypath)
 -- [[ Configure plugins ]]
 require('lazy').setup({
   {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup()
+    end,
+  },
+  {
     "folke/twilight.nvim",
     opts = {
       -- your configuration comes here
@@ -1499,23 +1505,17 @@ require('nvim-ts-autotag').setup({
     enable_rename = true,         -- Auto rename pairs of tags
     enable_close_on_slash = false -- Auto close on trailing </
   },
-  -- Also override individual filetype configs, these take priority.
-  -- Empty by default, useful if one of the "opts" global settings
-  -- doesn't work well in a specific filetype
-  -- per_filetype = {
-  --   ["html"] = {
-  --     enable_close = false
-  --   }
-  -- }
 })
 
 -- twilight.nvim
 vim.keymap.set("n", "<leader>tw", "<cmd>Twilight<CR>")
 
--- enable twilight by default 
+-- enable twilight by default
 vim.api.nvim_create_augroup('Twilight', { clear = true })
 vim.api.nvim_create_autocmd('BufEnter', {
   group = 'Twilight',
   pattern = '*',
   command = 'TwilightEnable'
 })
+
+-- nvim-colorizer.lua
