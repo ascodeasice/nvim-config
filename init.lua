031a1ad1f1af66c8edec019038f6ff1323782666
Leo Wang <ab0988956087@gmail.com>
2024-08-27 14:42:46 +0800

    add keybinding for selecting and show icon in nvim-cmp

--- init.lua
+++ init.lua
@@ -390,7 +390,7 @@ require('lazy').setup({
   },
   {
     "nvimtools/none-ls.nvim",
-    event="VeryLazy",
+    event = "VeryLazy",
     dependencies = {
       "gbprod/none-ls-shellcheck.nvim",
     },
@@ -1174,7 +1174,44 @@ require('luasnip.loaders.from_vscode').lazy_load()
 require("vim-react-snippets").lazy_load()
 luasnip.config.setup {}
 
+local cmp_kinds = {
+  Text = '',
+  Method = '',
+  Function = '',
+  Constructor = '',
+  Field = '',
+  Variable = '',
+  Class = '',
+  Interface = '',
+  Module = '',
+  Property = '',
+  Unit = '',
+  Value = '',
+  Enum = '',
+  Keyword = '',
+  Snippet = '',
+  Color = '',
+  File = '',
+  Reference = '',
+  Folder = '',
+  EnumMember = '',
+  Constant = '',
+  Struct = '',
+  Event = '',
+  Operator = '',
+  TypeParameter = '',
+}
+
 cmp.setup {
+  formatting = {
+    fields = { "kind", "abbr", "menu" },
+    format = function(_, vim_item)
+      -- remove the text after icon
+      vim_item.menu = "    (" .. (vim_item.kind or "") .. ")"
+      vim_item.kind = (cmp_kinds[vim_item.kind] or '')
+      return vim_item
+    end,
+  },
   snippet = {
     expand = function(args)
       luasnip.lsp_expand(args.body)
@@ -1187,6 +1224,26 @@ cmp.setup {
     ['<Down>'] = cmp.mapping.select_next_item(),
     ['<Up>'] = cmp.mapping.select_prev_item(),
     ['<C-Space>'] = cmp.mapping.complete {},
+    ["<C-Up>"] = function(fallback)
+      for i = 1, 5 do
+        cmp.mapping.select_prev_item()(nil)
+      end
+    end,
+    ["<C-Down>"] = function(fallback)
+      for i = 1, 5 do
+        cmp.mapping.select_next_item()(nil)
+      end
+    end,
+    ["<PageUp>"] = function(fallback)
+      for i = 1, 10 do
+        cmp.mapping.select_prev_item()(nil)
+      end
+    end,
+    ["<PageDown>"] = function(fallback)
+      for i = 1, 10 do
+        cmp.mapping.select_next_item()(nil)
+      end
+    end,
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
