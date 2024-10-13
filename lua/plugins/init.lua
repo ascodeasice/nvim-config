b1bb8ede3c3b4307df72af9479afbbd6053023f0
ascodeasice <ascodeasice@gmail.com>
2024-10-13 10:42:03 +0800

    enable nvim-cmp in comments

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1026,8 +1026,10 @@ require('lazy').setup({
 					if vim.api.nvim_get_mode().mode == 'c' then
 						return true
 					else
-						return not context.in_treesitter_capture("comment")
-								and not context.in_syntax_group("Comment")
+						-- NOTE: try enabling auto completion in comments
+						-- return not context.in_treesitter_capture("comment")
+						-- 		and not context.in_syntax_group("Comment")
+						return true
 					end
 				end
 			}
