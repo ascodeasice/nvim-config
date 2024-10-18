dad926eccfbd038f06f8c69e18b3e2bd03c73bfc
ascodeasice <ascodeasice@gmail.com>
2024-10-18 17:53:57 +0800

    do not render image for svg

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -250,7 +250,7 @@ require('lazy').setup({
 				editor_only_render_when_focused = true, -- auto show/hide images when the editor gains/looses focus
 				tmux_show_only_in_active_window = true, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
 				max_height_window_percentage = 50,
-				hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.svg" },
+				hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", },
 			})
 		end
 	}
