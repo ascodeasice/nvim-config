3a8dff294c5b78293e6b564c23cb72ae775c9325
ascodeasice <ascodeasice@gmail.com>
2024-12-23 12:44:34 +0800

    add sticky yank

--- init.lua
+++ init.lua
@@ -1385,3 +1385,23 @@ vim.api.nvim_create_autocmd("VimLeave", {
     os.execute("tmux resize-pane -Z") -- try to exit full screen of tmux
   end,
 })
+
+-- sticky yank
+local cursorPreYank
+vim.keymap.set({ "n", "x" }, "y", function()
+  cursorPreYank = vim.api.nvim_win_get_cursor(0)
+  return "y"
+end, { expr = true })
+vim.keymap.set("n", "Y", function()
+  cursorPreYank = vim.api.nvim_win_get_cursor(0)
+  return "y$"
+end, { expr = true })
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    if vim.v.event.operator == "y" and cursorPreYank then
+      vim.api.nvim_win_set_cursor(0, cursorPreYank)
+    end
+  end,
+})
+
