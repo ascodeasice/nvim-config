6a85eb4de6a8803bdd12b9b13ec662a9d900b6ec
ascodeasice <ascodeasice@gmail.com>
2025-01-06 03:35:06 +0800

    Revert "auto set cwd to where .git or Makefile is"
    This reverts commit 24091394c871ee47a59c5e5cd99f4b1f0a0b6aca.

--- lua/core/autocmds.lua
+++
@@ -1,9 +0,0 @@
--- set root to where .git or Makefile is
--- https://nanotipsforvim.prose.sh/automatically-set-the-cwd-without-rooter-plugin
-
-vim.api.nvim_create_autocmd("BufEnter", {
-	callback = function(ctx)
-		local root = vim.fs.root(ctx.buf, {".git", "Makefile"})
-		if root then vim.uv.chdir(root) end
-	end,
-})
