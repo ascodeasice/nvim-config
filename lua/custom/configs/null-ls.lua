ac9156dd885edd7da478ff33d57fb228d533303b
ascodeasice <ascodeasice@gmail.com>
2025-03-17 17:14:43 +0800

    disable line length rule for markdownlint

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -15,7 +15,9 @@ local opts = {
 		null_ls.builtins.formatting.shfmt.with({ filetypes = { "sh", "zsh" } }),
 		null_ls.builtins.formatting.prettierd,
 		null_ls.builtins.diagnostics.actionlint,
-		null_ls.builtins.diagnostics.markdownlint,
+		null_ls.builtins.diagnostics.markdownlint.with({
+			extra_args = { "--disable", "MD013", "--" },
+		}),
 	}
 }
 return opts
