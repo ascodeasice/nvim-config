943e93b859444b1a4cd5312eb08ef8587fc0603c
ascodeasice <ascodeasice@gmail.com>
2025-08-01 20:59:08 +0800

    stop using shellcheck for .env files

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -6,3 +6,12 @@ vim.api.nvim_create_autocmd("BufEnter", {
     -- vim.cmd("MoltenInit python3") -- use the system python kernel by default
   end,
 })
+
+local group = vim.api.nvim_create_augroup("__env", {clear = true})
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+  pattern = { "*.env", ".env.*" },
+  group = group,
+  callback = function()
+    vim.diagnostic.enable(false)
+  end,
+})
